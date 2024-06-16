f3422019409b53dce926b31d87ab3f373060f9af
iklod <18175451+iklod@users.noreply.github.com>
2024-06-16 13:10:24 +0200

    introduce slicing and range loop

--- arrslice/sum.go
+++ arrslice/sum.go
@@ -7,3 +7,29 @@ func Sum(arr []int) int {
 	}
 	return sum
 }
+
+func SumAll(arrToSum ...[]int) []int {
+	// lengthOfNumbers := len(arrToSum)
+	// sums := make([]int, lengthOfNumbers)
+	var sums []int
+	for _, numbers := range arrToSum {
+		// sums[i] = Sum(numbers)
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(arrToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range arrToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tails := numbers[1:]
+			sums = append(sums, Sum(tails))
+		}
+	}
+
+	return sums
+}
