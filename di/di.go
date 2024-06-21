6efa5dde71fc4dab5c8515e3cb2216f122a607e7
iklod <18175451+iklod@users.noreply.github.com>
2024-06-21 09:57:55 +0200

    init dependency injection

---
+++ di/di.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
+
+// The we can use it to print to console like this:
+// Greet(is.Stdout, "Elodie")
