c4e6c074a2a893d493012684a77d8bd4e5ac8fd8
iklod <18175451+iklod@users.noreply.github.com>
2024-06-23 19:22:39 +0200

    add pointer receiver for tests

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -11,7 +11,7 @@ type Rectangle struct {
 	Height float64
 }
 
-func (r Rectangle) Area() float64 {
+func (r *Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -20,7 +20,7 @@ func TestArea(t *testing.T) {
 		shape Shape
 		want  float64
 	}{
-		"Rectangle": {shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		"Rectangle": {shape: &Rectangle{Width: 12, Height: 6}, want: 72.0},
 		"Circle":    {shape: Circle{Radius: 10}, want: 314.1592653589793},
 		"Triangle":  {shape: Triangle{Base: 10, Height: 3}, want: 15},
 	}
